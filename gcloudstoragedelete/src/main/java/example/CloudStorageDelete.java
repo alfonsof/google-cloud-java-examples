03a1c9daa8c9afbaf9142569f203d6802f81471a
alfonsof <alfonsofb@gmail.com>
2018-09-27 14:50:50 +0200

    Improved print messages

--- gcloudstoragedelete/src/main/java/example/CloudStorageDelete.java
+++ gcloudstoragedelete/src/main/java/example/CloudStorageDelete.java
@@ -1,23 +1,22 @@
 /**
  * CloudStorageDelete is an example that handles Cloud Storage buckets on GCP (Google Cloud Platform)
- * Delete a Google Storage bucket for a Google Cloud Project.
+ * Delete a Google Storage bucket in a Google Cloud Project.
  * The application uses Application Default Credentials through a JSON service account key for authenticating.
  * The credentials are taken from GOOGLE_APPLICATION_CREDENTIALS environment variable.
- * You must use 1 parameter:
- * BUCKET = Name of bucket
+ * You must provide 1 parameter:
+ * BUCKET_NAME = Name of the bucket
  */
 
 package example;
 
 import com.google.cloud.storage.Bucket;
-import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 
 public class CloudStorageDelete {
     public static void main(String[] args) {
         if (args.length < 1) {
-            System.out.println("Not enough parameters. Proper Usage is: java -jar cloudstoragecreate.jar <BUCKET_NAME>");
+            System.out.println("Not enough parameters.\nProper Usage is: java -jar cloudstoragedelete.jar <BUCKET_NAME>");
             System.exit(1);
         }
 
@@ -27,20 +26,21 @@ public class CloudStorageDelete {
         // The name for the new bucket
         String bucketName = args[0];
 
+        System.out.printf("Bucket name: %s\n", bucketName);
+
         Bucket bucket = storage.get(bucketName);
 
-        if (bucket == null) {
-            // Not exists the bucket
-            System.out.println("No such bucket");
-            System.exit(1);
-        }
-
-        // Delete the bucket
-        boolean deleted = bucket.delete(Bucket.BucketSourceOption.metagenerationMatch());
-        if (deleted) {  // the bucket was deleted
-            System.out.printf("Bucket %s Deleted.\n", bucket.getName());    
-        } else {    // the bucket was not found
-            System.out.printf("Bucket %s NOT Deleted.\n", bucket.getName());    
+        if (bucket != null) {   // Exists the bucket
+            System.out.println("Deleting bucket ...");
+            // Delete the bucket
+            boolean deleted = bucket.delete(Bucket.BucketSourceOption.metagenerationMatch());
+            if (deleted) {  // the bucket was deleted
+                System.out.printf("Bucket \"%s\" Deleted\n", bucket.getName());
+            } else {    // the bucket was not found
+                System.out.printf("Error: Bucket \"%s\" NOT Deleted.\n", bucket.getName());
+            }
+        } else {   // Not exists the bucket
+            System.out.println("Error: Bucket does not exist!!");
         }
     }
 }
